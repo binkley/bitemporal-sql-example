25c8736e9dfc3eb898ae541c09e8bf3da960db37
Brian Oxley <boxley@thoughtworks.com>
2021-04-10 11:13:29 +0800

    Sproc, formatting

--- docker-entrypoint-initdb.d/02-procs.sh
+++ docker-entrypoint-initdb.d/02-procs.sh
@@ -2,5 +2,21 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 "$POSTGRES_DB" "$POSTGRES_USER" <<EOSQL
+psql -v ON_ERROR_STOP=1 "$POSTGRES_DB" "$POSTGRES_USER" <<'EOSQL'
+CREATE PROCEDURE example_as_of (
+    as_of TIMESTAMP
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+CREATE VIEW example_latest AS
+    SELECT * from example_p
+    WHERE (key, applies_to, effective_at) IN (
+        SELECT key, applies_to, max(effective_at)
+        FROM example_p
+        WHERE effective_at <= as_of
+        GROUP BY key, applies_to
+    );
+END;
+$$
 EOSQL
