4dffc66f653e2a58fa209eafb843792c25eea2b2
Brian Oxley <boxley@thoughtworks.com>
2021-04-12 10:31:43 +0800

    Style

--- docker-entrypoint-initdb.d/03-data.sql
+++ docker-entrypoint-initdb.d/03-data.sql
@@ -1,7 +1,9 @@
 INSERT INTO example_p (key, value, applies_to, effective_at)
 VALUES ('A', 3, '2021-02-01', '2021-02-01');
+-- An example of deciding a fact which takes effect in the future
 INSERT INTO example_p (key, value, applies_to, effective_at)
-VALUES ('B', 17, '2021-02-01', '2021-02-01');
+VALUES ('B', 17, '2021-02-01', '2021-01-15');
+-- Two examples of revising a fact previously decided
 INSERT INTO example_p (key, value, applies_to, effective_at)
 VALUES ('A', 4, '2021-02-01', '2021-02-02');
 INSERT INTO example_p (key, value, applies_to, effective_at)
